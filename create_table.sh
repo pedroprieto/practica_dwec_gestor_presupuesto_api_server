42d8a5947898004f71c1911e3b527f67f6d0ebfa
Pedro Prieto <pedpral@gmail.com>
2021-12-13 13:15:02 +0100

    Aplicación funcionando

---
+++ create_table.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+HASH_KEY="usuario"
+SORT_KEY="gastoId"
+aws dynamodb create-table --table-name $1 \
+    --attribute-definitions AttributeName=$HASH_KEY,AttributeType=S AttributeName=$SORT_KEY,AttributeType=N \
+    --key-schema AttributeName=$HASH_KEY,KeyType=HASH AttributeName=$SORT_KEY,KeyType=RANGE \
+    --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
+    --region eu-west-1 \
+    --query TableDescription.TableArn --output text
+
+# Wait for table to exist
+aws dynamodb wait table-exists --table-name $1
---
+++ index.js
@@ -0,0 +1,92 @@
+const ApiBuilder = require('claudia-api-builder'),
+      querystring = require('querystring'),
+      AWS = require('aws-sdk');
+
+var api = new ApiBuilder(),
+    dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+const tableName = process.env.TABLE_NAME;
+
+function mostrarGastos(usuario, gastoId) {
+
+    var condExp = (gastoId == undefined) ? 'usuario = :usuario' : 'usuario = :usuario and gastoId = :gastoId';
+
+    var expAttrs = {
+        ':usuario' : usuario
+    };
+
+    if (gastoId != undefined)
+        expAttrs[':gastoId'] = gastoId;
+
+
+    var params = {
+        ExpressionAttributeValues: expAttrs,
+        KeyConditionExpression: condExp,
+        TableName: tableName
+    };
+    return dynamoDb.query(params).promise()
+        .then(response => {
+            return response.Items;
+        })
+}
+
+// Gastos del usuario (GET)
+api.get('/{usuario}', function (request) {
+    var usuarioNombre = querystring.unescape(request.pathParams.usuario);
+    return mostrarGastos(usuarioNombre);
+
+});
+
+// POST
+api.post('/{usuario}', function (request) {
+    var data = request.body;
+    var usuarioNombre = querystring.unescape(request.pathParams.usuario);
+    data.usuario = usuarioNombre;
+    var params = {
+        TableName: tableName,
+        Item: data
+    }
+    return dynamoDb.put(params).promise();
+}, { success: 201 });
+
+
+// Gasto de un usuario (GET)
+api.get('/{usuario}/{gastoId}', function (request) {
+    var usuarioNombre = querystring.unescape(request.pathParams.usuario);
+    var gastoId = parseInt(querystring.unescape(request.pathParams.gastoId));
+    return mostrarGastos(usuarioNombre, gastoId).then(items => {
+        return (items.length>0) ? items[0] : {};
+    });
+
+});
+
+// PUT
+api.put('/{usuario}/{gastoId}', function (request) {
+    var data = request.body;
+    var usuarioNombre = querystring.unescape(request.pathParams.usuario);
+    var gastoId = parseInt(querystring.unescape(request.pathParams.gastoId));
+    data.usuario = usuarioNombre;
+    data.gastoId = gastoId;
+    var params = {
+        TableName: tableName,
+        Item: data
+    }
+    return dynamoDb.put(params).promise();
+}, { success: 200 });
+
+// DELETE
+api.delete('/{usuario}/{gastoId}', function (request) {
+    var usuarioNombre = querystring.unescape(request.pathParams.usuario);
+    var gastoId = parseInt(querystring.unescape(request.pathParams.gastoId));
+    var params = {
+        TableName: tableName,
+        Key: {
+            usuario: usuarioNombre,
+            gastoId: gastoId
+        }
+    }
+    return dynamoDb.delete(params).promise();
+});
+
+
+module.exports = api;
